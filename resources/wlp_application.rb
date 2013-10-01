c6396347b7099e845138eae72201c0f6f86ae651
ant <ant.elder@uk.ibm.com>
2013-10-01 11:05:16 +0100

    Add license/copyright headers

--- resources/wlp_application.rb
+++ resources/wlp_application.rb
@@ -2,6 +2,8 @@
 # Cookbook Name:: application_wlp
 # Resource:: wlp_application
 #
+# © Copyright IBM Corporation 2013.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
