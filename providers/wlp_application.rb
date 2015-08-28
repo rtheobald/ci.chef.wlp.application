4cdc3059ca7308d161ad44b1e51e16ffc06a3dec
rtheobald <rtheobald@virttrade.com>
2015-08-28 15:01:26 +0100

    Install fails in java recipe

--- providers/wlp_application.rb
+++ providers/wlp_application.rb
@@ -20,7 +20,7 @@
 include Chef::Mixin::LanguageIncludeRecipe
 
 action :before_compile do
-  include_recipe "wlp"
+  include_recipe "wlp_ibm"
 
   wlp_server new_resource.server_name do
     config (new_resource.server_config)
